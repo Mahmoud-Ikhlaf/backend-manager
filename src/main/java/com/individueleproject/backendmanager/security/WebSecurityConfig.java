73a28c06c1cf504cd08bd65a25a6a5d6320e84fc
Mahmoud Ikhlaf <513095@student.fontys.nl>
2023-12-01 15:06:05 +0100

    Updated configuration for swagger on production

--- src/main/java/com/individueleproject/backendmanager/security/WebSecurityConfig.java
+++ src/main/java/com/individueleproject/backendmanager/security/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig {
                         authorizeRequests
                             .requestMatchers("/api/v1/auth/**").permitAll()
                             .requestMatchers("/swagger-ui/**").permitAll()
-                            .requestMatchers("/v3/api-docs").permitAll()
+                            .requestMatchers("/v3/api-docs/**").permitAll()
                             .anyRequest().authenticated()
                 )
                 .sessionManagement((sessionManagement) ->
