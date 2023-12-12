3ef49cfbd80b55580555b491fbb6c5bcbdba8355
Mahmoud Ikhlaf <513095@student.fontys.nl>
2023-12-12 14:37:35 +0100

    Added integration tests

---
+++ src/test/java/com/individueleproject/backendmanager/integration_tests/AuthIntegrationTest.java
@@ -0,0 +1,130 @@
+package com.individueleproject.backendmanager.integration_tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.individueleproject.backendmanager.BackendManagerApplication;
+import com.individueleproject.backendmanager.entity.User;
+import com.individueleproject.backendmanager.models.LoginRequest;
+import com.individueleproject.backendmanager.models.RegisterRequest;
+import com.individueleproject.backendmanager.repository.RefreshTokenRepository;
+import com.individueleproject.backendmanager.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = BackendManagerApplication.class)
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties"
+)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AuthIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private final String API_ENDPOINT = "/api/v1/auth";
+    private User user;
+    private LoginRequest loginRequest;
+    private RegisterRequest registerRequest;
+
+    @BeforeAll
+    public void setup() {
+        user = User.builder()
+                .id(1L)
+                .username("test")
+                .password(passwordEncoder.encode("test"))
+                .email("test@test.com")
+                .build();
+
+        loginRequest = LoginRequest.builder()
+                .username("test")
+                .password("test")
+                .build();
+
+        registerRequest = RegisterRequest.builder()
+                .username("test2")
+                .email("test2@test.com")
+                .password("test2")
+                .build();
+        userRepository.flush();
+        refreshTokenRepository.flush();
+        userRepository.save(user);
+    }
+
+    @Test
+    public void givenLoginCredential_whenLogin_thenStatus200() throws Exception {
+
+        mvc.perform(post(API_ENDPOINT + "/login")
+                .content(mapper.writeValueAsString(loginRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(containsStringIgnoringCase("accessToken")));
+    }
+
+    @Test
+    public void givenLoginCredential_whenLogin_thenStatus401() throws Exception {
+
+        mvc.perform(post(API_ENDPOINT + "/login")
+                        .content(mapper.writeValueAsString(LoginRequest.builder().build()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void givenRegisterData_whenRegister_thenStatus200() throws Exception {
+
+        mvc.perform(post(API_ENDPOINT + "/register")
+                .content(mapper.writeValueAsString(registerRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsStringIgnoringCase("Gebruiker is succesvol geregistreerd!")));
+    }
+
+    @Test
+    public void givenRegisterData_whenRegister_thenUsernameAlreadyExists() throws Exception {
+
+        mvc.perform(post(API_ENDPOINT + "/register")
+                .content(mapper.writeValueAsString(RegisterRequest.builder().username("test").password("test").email("test@test.com").build()))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsStringIgnoringCase("Gebruiksnaam bestaat al!")));
+    }
+
+    @Test
+    public void givenRegisterData_whenRegister_thenEmailAlreadyExists() throws Exception {
+
+        mvc.perform(post(API_ENDPOINT + "/register")
+                .content(mapper.writeValueAsString(RegisterRequest.builder().username("a").password("test").email("test@test.com").build()))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsStringIgnoringCase("Email bestaat al!")));
+    }
+
+}
--- src/test/java/com/individueleproject/backendmanager/integration_tests/SwaggerTest.java
+++ src/test/java/com/individueleproject/backendmanager/integration_tests/SwaggerTest.java
@@ -1,9 +1,14 @@
-package com.individueleproject.backendmanager.unit_tests.controllers;
+package com.individueleproject.backendmanager.integration_tests;
 
+import com.individueleproject.backendmanager.BackendManagerApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
@@ -11,21 +16,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = BackendManagerApplication.class)
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties"
+)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SwaggerTest {
 
     @Autowired
     private MockMvc api;
 
     @Test
-    void anyoneCanAccessSwaggerEndpoint() throws Exception {
+    public void anyoneCanAccessSwaggerEndpoint() throws Exception {
         api.perform(get("/swagger-ui/index.html#/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsStringIgnoringCase("Swagger UI")));
     }
 
     @Test
-    void anyoneCanAccessSwaggerApiDocsEndpoint() throws Exception {
+    public void anyoneCanAccessSwaggerApiDocsEndpoint() throws Exception {
         api.perform(get("/api-docs"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsStringIgnoringCase("Mahoot API")));
